a060fa74412446300ac011e6211e12bee1aae294
Kevin Lim <kclim@ucsc.edu>
2022-04-21 02:18:05 -0700

    Updates

---
+++ precache-manifest.9ddd69d855cfd6d4e9db6e68b7e47f47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "476ea5f5c133ab908938",
+    "url": "/starwars-dashboard/static/css/main.0a46459e.chunk.css"
+  },
+  {
+    "revision": "476ea5f5c133ab908938",
+    "url": "/starwars-dashboard/static/js/main.2f56fa4d.chunk.js"
+  },
+  {
+    "revision": "c09d0abb309cfd1c24c5",
+    "url": "/starwars-dashboard/static/js/runtime~main.6d084ce5.js"
+  },
+  {
+    "revision": "d0283925318717a59ed6",
+    "url": "/starwars-dashboard/static/js/2.a02d30a0.chunk.js"
+  },
+  {
+    "revision": "d8bd2ce65f318c4eb676",
+    "url": "/starwars-dashboard/static/js/3.451dfcd2.chunk.js"
+  },
+  {
+    "revision": "050dc84ce1d7150ef0672c1c5bd1ce21",
+    "url": "/starwars-dashboard/index.html"
+  }
+];
